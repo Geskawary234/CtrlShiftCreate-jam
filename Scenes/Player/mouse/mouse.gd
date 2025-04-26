62bc92ad1c69a7c7db2cbb5d4d957b220c0926d7
Geskawary234 <danilo2006dragovich@gmail.com>
2025-04-26 12:20:16 +0300

    mouse script change

--- Scenes/Player/mouse/mouse.gd
+++ Scenes/Player/mouse/mouse.gd
@@ -11,11 +11,11 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	
 	
-	if !is_on_floor():
+	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
